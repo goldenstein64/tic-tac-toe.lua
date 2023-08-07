3859e811ce6e4a0afb9941b4a784a384cfa5c634
goldenstein64 <goldnstein64@gmail.com>
2023-08-06 20:23:46 -0400

    first attempt at optimizing Minimax algorithm

--- tic-tac-toe/player/Computer.lua
+++ tic-tac-toe/player/Computer.lua
@@ -5,20 +5,117 @@ math.randomseed(os.time() / math.pi)
 
 local Computer = {}
 
+local symmetryPriority = {
+	"rotate90",
+	"rotate180",
+	"diagonalDown",
+	"diagonalUp",
+	"horizontal",
+	"vertical",
+}
+
+---@type number[][][]
+local symmetries = {
+	vertical = {
+		{ 1, 3 },
+		{ 4, 6 },
+		{ 7, 9 },
+	},
+	horizontal = {
+		{ 1, 7 },
+		{ 2, 8 },
+		{ 3, 9 },
+	},
+	diagonalUp = {
+		{ 2, 4 },
+		{ 3, 7 },
+		{ 6, 8 },
+	},
+	diagonalDown = {
+		{ 1, 9 },
+		{ 2, 6 },
+		{ 4, 8 },
+	},
+	rotate180 = {
+		{ 1, 9 },
+		{ 2, 8 },
+		{ 3, 7 },
+		{ 4, 6 },
+	},
+	rotate90 = {
+		{ 1, 3, 7, 9 },
+		{ 2, 4, 6, 8 },
+	},
+}
+
+local symmetryImages = {
+	vertical = { 1, 2, 4, 5, 7, 8 },
+	horizontal = { 1, 2, 3, 4, 5, 6 },
+	diagonalUp = { 1, 2, 3, 4, 5, 7 },
+	diagonalDown = { 1, 2, 3, 5, 6, 9 },
+	rotate180 = { 1, 2, 3, 4, 5 },
+	rotate90 = { 1, 2, 5 },
+}
+
+---@param board Board
+---@param symmetry number[][]
+---@return boolean
+local function symmetryMatches(board, symmetry)
+	for _, matches in ipairs(symmetry) do
+		local firstMark = board.board[matches[1]]
+		for i = 2, #matches do
+			if firstMark ~= board.board[matches[i]] then
+				return false
+			end
+		end
+	end
+
+	return true
+end
+
 ---@param board Board
 ---@return integer[]
----@nodiscard
-function Computer.actions(board)
+local function filterMarkableActions(board, ...)
 	local result = {}
 
-	for pos = 1, 9 do
-		local mark = board.board[pos]
-		if mark == nil then
+	for _, pos in ... do
+		if board:canMark(pos) then
 			table.insert(result, pos)
 		end
 	end
 
 	return result
+end
+
+---@param board Board
+---@return integer[]
+local function simpleActions(board)
+	local result = {}
+
+	for pos = 1, 9 do
+		if board:canMark(pos) then
+			table.insert(result, pos)
+		end
+	end
+
+	return result
+end
+
+---@param board Board
+---@return integer[]?
+local function symmetricActions(board)
+	for _, symmetryName in ipairs(symmetryPriority) do
+		if symmetryMatches(board, symmetries[symmetryName]) then
+			return filterMarkableActions(board, ipairs(symmetryImages[symmetryName]))
+		end
+	end
+end
+
+---@param board Board
+---@return integer[]
+---@nodiscard
+function Computer.actions(board)
+	return symmetricActions(board) or simpleActions(board)
 end
 
 ---@param board Board
@@ -88,12 +185,11 @@ end
 ---@nodiscard
 function Computer.getMoves(board, mark)
 	local opponentMark = mark:other()
-	local moves = Computer.actions(board)
 
 	local bestScore = Computer.controls[mark]
 	local reconcile = Computer.reconcilers[mark]
 	local bestMoves = {}
-	for _, position in ipairs(moves) do
+	for _, position in ipairs(simpleActions(board)) do
 		local nextBoard = Computer.resultOf(board, mark, position)
 		local nextScore = Computer.judge(nextBoard, opponentMark)
 		if nextScore == bestScore then
