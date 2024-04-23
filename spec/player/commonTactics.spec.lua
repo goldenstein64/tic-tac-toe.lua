7171a907714da34d6350f8b952004d4f93de9be4
goldenstein64 <goldnstein64@gmail.com>
2024-04-23 00:45:06 -0400

    test: common tactics uses Computer:getMoves

--- spec/player/commonTactics.spec.lua
+++ spec/player/commonTactics.spec.lua
@@ -4,19 +4,19 @@ local Mark = require("tic-tac-toe.data.Mark")
 local METHOD_FORMAT = "%s:getMove"
 
 local testTactics = {
-	{ "detects winning moves for X", ",XX,OO,,,", Mark.X, 1 },
-	{ "detects winning moves for O", ",OO,XX,,X", Mark.O, 1 },
-	{ "detects blocking moves for X", "O,,O,X,X,", Mark.X, 7 },
-	{ "detects blocking moves for O", ",O,X,,XXO", Mark.O, 1 },
-	{ "detects trapping moves for X", ",X,O,X,O,", Mark.X, 3 },
-	{ "detects trapping moves for O", ",X,OXX,O,", Mark.O, 7 },
-	{ "matches ',XXXOOOX,'", ",XXXOOOX,", Mark.O, 1 },
+	{ "detects winning moves for X", ",XX,OO,,,", Mark.X, { 1 } },
+	{ "detects winning moves for O", ",OO,XX,,X", Mark.O, { 1 } },
+	{ "detects blocking moves for X", "O,,O,X,X,", Mark.X, { 7 } },
+	{ "detects blocking moves for O", ",O,X,,XXO", Mark.O, { 1 } },
+	{ "detects trapping moves for X", ",X,O,X,O,", Mark.X, { 3 } },
+	{ "detects trapping moves for O", ",X,OXX,O,", Mark.O, { 7 } },
+	{ "matches ',XXXOOOX,'", ",XXXOOOX,", Mark.O, { 1 } },
 }
 
----@param computerGen fun(): tic-tac-toe.Player
+---@param computerGen fun(): tic-tac-toe.Computer
 ---@param name string
 local function testCommonTactics(computerGen, name)
-	local computer
+	local computer ---@type tic-tac-toe.Computer
 	before_each(function()
 		computer = computerGen()
 	end)
@@ -28,9 +28,9 @@ local function testCommonTactics(computerGen, name)
 			it(testName, function()
 				local board = Board.fromPattern(pattern)
 
-				local move = computer:getMove(board, mark)
+				local move = computer:getMoves(board, mark)
 
-				expect(move).to.equal(expected)
+				expect(move).to.look.like(expected)
 			end)
 		end
 	end)
