41c2ed43f464fa67b32cea487b004642f01ca6c8
goldenstein64 <goldnstein64@gmail.com>
2025-05-30 16:34:07 -0400

    fix: split math.randomseed call by lua version

--- tic-tac-toe-console/init.lua
+++ tic-tac-toe-console/init.lua
@@ -1,9 +1,13 @@
 ---a console application of tic-tac-toe
 
-math.randomseed(os.time() / math.exp(1), os.time() / math.pi)
+if _VERSION == "Lua 5.3" or _VERSION == "Lua 5.4" then
+	math.randomseed(math.floor(os.time() / math.exp(1)))
+else
+	math.randomseed(os.time() / math.exp(1), os.time() / math.pi)
+end
 
 local ConsoleConnection = require("tic-tac-toe-console.ConsoleConnection")
-local App = require("tic-tac-toe")
+local App = require("tic-tac-toe.init")
 
 local conn = ConsoleConnection()
 local app = App(conn)
